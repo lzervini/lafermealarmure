3f8154db1dd23d91d2d0654827e015f7b47a3bda
Léa <lezervinia@gmail.com>
2023-01-07 22:04:45 +0100

    Change the plugin for google review

--- front-page.php
+++ front-page.php
@@ -4,9 +4,10 @@
  	if ( have_posts() ) :
  		while ( have_posts() ) : the_post();
 
- 			the_title( '<h1>', '</h1>' );
+		 the_title( '<h1>', '</h1>' );
+ 			the_post_thumbnail( );
  			the_content('');
-			  echo do_shortcode( '[grw id="40"]' ); 
+			  echo do_shortcode( '[trustindex no-registration=google]' ); 
 			  echo '<h3>' . get_post_type() . '</h3>';
 			  +			get_template_part( 'template-parts/services.php', get_post_type() );
  		endwhile;
