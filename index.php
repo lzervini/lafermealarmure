18dcb5c1839fbe9bc7520f456f36068dbd20fe0a
Léa <lezervinia@gmail.com>
2023-01-14 22:34:32 +0100

    Ajout de div

--- index.php
+++ index.php
@@ -10,12 +10,14 @@
 <?php get_header(); ?>
  <?php
  	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
+		while ( have_posts() ) : the_post();?>
 
-			the_title( '<h1>', '</h1>' );
-			the_content('');
-			echo '<h3>' . get_post_type() . '</h3>';
-+			get_template_part( 'template-parts/services.php', get_post_type() );
+			
+			<div class="card-region">
+
+				<?php the_content();?>
+			</div>
+			<?php get_template_part('template-parts','services');
 
 		endwhile;
 	endif;
