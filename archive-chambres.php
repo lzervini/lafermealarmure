ffaf897eb61c7b2e45d03d75f6477ddb0a4249ce
Léa <lezervinia@gmail.com>
2023-01-15 19:19:05 +0100

    Ajout des éléments pour l'affichage de la page d'archive

--- archive-chambres.php
+++ archive-chambres.php
@@ -1,14 +1,27 @@
 <?php get_header(); ?>
+<div class="card_all">
 
 <?php
  	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
+		while ( have_posts() ) : the_post();?>
+		<article class="card_middle">
+			<div class="card_top">
+				<p> TEST</p>
+			</div>
+			<div class="card_bottom">
+				<?php the_title( '<h2>', '</h2>' ); ?>
+				<!-- <?php the_post_thumbnail( '' ); ?> -->
+				<p ><?php echo get_the_excerpt(); ?></p>
+				<div class="wp-block-button aligncetner main-button">
+					<a class="wp-block-button__link" href="<?php the_permalink(); ?>"> Voir la chambre</a>
+				</div>
+			</div>
+		</article>
 
-			the_title( '<h1>', '</h1>' );
-			the_content('');
 
-		endwhile;
+	<?php	endwhile;
 	endif;
  ?>
+</div>
 
 <?php get_footer(); ?>
