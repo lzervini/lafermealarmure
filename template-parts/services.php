6d479424f2291ef28b7dffbb99d3b0e767d3b9e7
Léa <lezervinia@gmail.com>
2023-01-16 23:36:11 +0100

    Test d'insertion du répéteur - Echec

--- template-parts/services.php
+++ template-parts/services.php
@@ -1,10 +1,22 @@
+<?php 
+
+$icone    = get_sub_field('icone_service', 'infos');
+$title    = get_field('titre_services', 'infos');
+$text    = get_field('texte_services', 'infos');
+
+if( have_rows( 'services', 'infos' ) ): ?>
+<div class="essai">
+  <?php while(have_rows('services', 'infos')): the_row(); ?>
+  <img src="<?php echo $icone['url']; ?>"></img>  
+<h2> <?php   echo $title; ?></h2>
+<p> <?php echo $text; ?></p>
 <?php
+			
 
-  $icone    = get_field('icone_service', 'infos');
-  $title    = get_field('titre_services', 'infos');
-  $text    = get_field('texte_services', 'infos');
-
+  echo $icone;
+  echo $title;
+  echo $text;
+endwhile;
+endif;
 ?>
 
-<img src="<?php echo $icone_service['url']; ?>" class="style-svg"/>
-<p class="test">TEST</p>
