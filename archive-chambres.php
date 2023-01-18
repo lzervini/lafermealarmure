624b29da434ffb9941b55ce85934a44d4af917dc
Léa <lezervinia@gmail.com>
2023-01-19 00:17:36 +0100

    Stylisation de la page archive

--- archive-chambres.php
+++ archive-chambres.php
@@ -5,15 +5,12 @@
  	if ( have_posts() ) :
 		while ( have_posts() ) : the_post();?>
 		<article class="card_middle">
-			<div class="card_top">
-				<p> TEST</p>
-			</div>
-			<div class="card_bottom">
-				<?php the_title( '<h2>', '</h2>' ); ?>
-				<!-- <?php the_post_thumbnail( '' ); ?> -->
-				<p ><?php echo get_the_excerpt(); ?></p>
-				<div class="wp-block-button aligncetner main-button">
-					<a class="wp-block-button__link" href="<?php the_permalink(); ?>"> Voir la chambre</a>
+		<img class="img-card" src="<?php echo catch_that_image()?>"></img>
+			<div class="card_text">
+			<?php the_title( '<h2>', '</h2>' );?>
+			<p><?php echo get_the_excerpt(); ?></p>
+			<div class="wp-block-button aligncetner main-button flex-self-end">
+					<a class="wp-block-button__link " href="<?php the_permalink(); ?>"> Voir la chambre</a>
 				</div>
 			</div>
 		</article>
